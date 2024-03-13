ff4c0a0cdce4acfbcc6ffb164f11f7cb68f2593e
computerclubsystem <a@a.a>
2024-03-14 01:34:38 +0200

    Always run the task for trying power on output pin

--- DeviceTimerApp/App.cs
+++ DeviceTimerApp/App.cs
@@ -1,6 +1,5 @@
 using System.Device.Gpio;
 using DeviceTimer.IOController;
-using DeviceTimerApp;
 
 namespace DeviceTimer.DeviceTimerApp;
 
@@ -15,41 +14,21 @@ public class App
         _state = CreateAppState();
         _gpio = CreateRaspberryPi5Gpio();
         ConfigurePins();
-        // RegisterPinValueChangedCallbacks();
         _psPower = new PlayStationPower(_gpio, _state.StartPlayStationPowerInputPin, _state.StartPlayStationPowerOutputPin);
-        // _psPower.StartMonitoring();
-        // SetStartPlayStationPowerOutputPinValue(false);
         // TODO: Start WebSocket
-        // TODO: Start the GPIO flow and wait for the cancellation token
         StartGpioFlow();
-        await Task.Delay(Timeout.InfiniteTimeSpan, _state.CancellationToken);
+        try
+        {
+            await Task.Delay(Timeout.InfiniteTimeSpan, _state.CancellationToken);
+        }
+        catch { }
         CleanUp();
     }
 
-    private async void StartGpioFlow()
+    private void StartGpioFlow()
     {
         _psPower.StartMonitoring();
-        // await Task.Factory.StartNew(async () =>
-        // {
-        //     string state = "power-up-playstation";
-        //     while (!_state.CancellationToken.IsCancellationRequested)
-        //     {
-        //         if (state == "power-up-playstation")
-        //         {
-        //             _gpio.SetOutputPinValue(_state.StartPlayStationPowerOutputPinNumber, false);
-        //             await Task.Delay(TimeSpan.FromSeconds(5), _state.CancellationToken);
-        //             _gpio.SetOutputPinValue(_state.StartPlayStationPowerOutputPinNumber, true);
-        //             await Task.Delay(TimeSpan.FromSeconds(1), _state.CancellationToken);
-        //         }
-        //     }
-        // }, _state.CancellationToken);
     }
-
-
-    // private void SetStartPlayStationPowerOutputPinValue(bool value)
-    // {
-    //     _gpio.SetOutputPinValue(_state.StartPlayStationPowerOutputPinNumber, value);
-    // }
 
     private void ConfigurePins()
     {
@@ -58,26 +37,13 @@ public class App
         _state.StartPlayStationPowerInputPin = _gpio.ConfigurePinAsInput(_state.StartPlayStationPowerInputPinNumber, PinMode.InputPullDown);
     }
 
-    // private void RegisterPinValueChangedCallbacks()
-    // {
-    //     _gpio.RegisterCallbackForPinValueChangedEvent(
-    //         _state.StartPlayStationPowerInputPinNumber,
-    //          PinEventTypes.Rising,
-    //          OnPlayStationPowerInputPinValueChanged
-    //     );
-    // }
-
-    // private void OnPlayStationPowerInputPinValueChanged(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
-    // {
-    //     Console.WriteLine("{0} : PlayStationPowerInputPin value {1}", DateTime.Now.ToString("s"), pinValueChangedEventArgs.ChangeType);
-    // }
-
     private RaspberryPi5Gpio CreateRaspberryPi5Gpio()
     {
         RaspberryPi5Gpio rpi5Gpio = new();
         rpi5Gpio.Init();
         return rpi5Gpio;
     }
+
     private AppState CreateAppState()
     {
 
@@ -95,6 +61,7 @@ public class App
 
     private void CleanUp()
     {
+        _psPower.Dispose();
         _gpio.Dispose();
     }
 
--- DeviceTimerApp/PlayStationPower.cs
+++ DeviceTimerApp/PlayStationPower.cs
@@ -1,70 +1,121 @@
 using System.Device.Gpio;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using DeviceTimer.IOController;
 
-namespace DeviceTimerApp;
+namespace DeviceTimer.DeviceTimerApp;
 
 public class PlayStationPower
 {
     private readonly RaspberryPi5Gpio gpio;
     private readonly GpioPin outputPin;
     private readonly GpioPin inputPin;
-    private CancellationTokenSource powerOnCancellationTokenSource;
+    private CancellationTokenSource tryPowerOnCancellationTokenSource;
     private Task powerOnTask;
+    private readonly Subject<PinValueChangedEventArgs> inputPinValueChangesSubject;
+    private readonly IObservable<PinValueChangedEventArgs> inputPinValueChangesObservable;
+    private readonly Subject<PinEventTypes> inputPinValueChangeDetectedSubject;
+    private readonly object lockObject = new();
+
+    private IDisposable inputPinValueChangesSubscription;
+    private readonly TimeSpan inputPinThrottleTimeSpan = TimeSpan.FromSeconds(1);
 
     public PlayStationPower(RaspberryPi5Gpio gpio, GpioPin inputPin, GpioPin outputPin)
     {
         this.gpio = gpio;
         this.outputPin = outputPin;
         this.inputPin = inputPin;
+        inputPinValueChangesSubject = new();
+        inputPinValueChangesObservable = inputPinValueChangesSubject.AsObservable();
+        inputPinValueChangeDetectedSubject = new();
+    }
+
+    public IObservable<PinEventTypes> GetPowerButtonChangeDetectedObservable()
+    {
+        return inputPinValueChangeDetectedSubject.AsObservable();
     }
 
     public void StartMonitoring()
     {
+        inputPinValueChangesSubscription = inputPinValueChangesObservable
+            .Throttle(inputPinThrottleTimeSpan)
+            .Subscribe(OnPowerInputPinChangeDetected);
         gpio.RegisterCallbackForPinValueChangedEvent(
             inputPin.PinNumber,
             PinEventTypes.Rising | PinEventTypes.Falling,
             OnPlayStationPowerInputPinValueChanged
         );
-        powerOnCancellationTokenSource = new CancellationTokenSource();
-        powerOnTask = Task.Factory.StartNew(PowerOnAction, powerOnCancellationTokenSource.Token);
+        tryPowerOnCancellationTokenSource = new CancellationTokenSource();
+        powerOnTask = Task.Factory.StartNew(TryPowerOnAction, tryPowerOnCancellationTokenSource.Token);
+    }
+
+    private void OnPowerInputPinChangeDetected(PinValueChangedEventArgs args)
+    {
+        inputPinValueChangeDetectedSubject.OnNext(args.ChangeType);
+        if (args.ChangeType == PinEventTypes.Rising)
+        {
+            SetTryPowerOnOutputPinValue(PinValue.Low);
+        }
+        Console.WriteLine("PS power button value changed {0}", args.ChangeType);
+    }
+
+    private void SetTryPowerOnOutputPinValue(PinValue pinValue)
+    {
+        lock (lockObject)
+        {
+            outputPin.Write(pinValue);
+        }
     }
 
     public void StopMonitoring()
     {
-        powerOnCancellationTokenSource.Cancel();
-        if (powerOnTask != null)
-        {
-            Task.WaitAll([powerOnTask]);
-        }
         gpio.UnregisterCallbackForPinValueChangedEvent(inputPin.PinNumber, OnPlayStationPowerInputPinValueChanged);
+        SetTryPowerOnOutputPinValue(PinValue.Low);
     }
 
     public void Dispose()
     {
-        // TODO: Stop the threads, unsubscribe etc.
+        // Stop the threads, unsubscribe etc.
+        inputPinValueChangesSubscription?.Dispose();
+        try
+        {
+            tryPowerOnCancellationTokenSource.Cancel();
+        }
+        catch { }
         StopMonitoring();
     }
 
-    private async void PowerOnAction()
+    private async void TryPowerOnAction()
     {
-        while (!powerOnCancellationTokenSource.IsCancellationRequested)
+        while (!tryPowerOnCancellationTokenSource.IsCancellationRequested)
         {
-            if (!powerOnCancellationTokenSource.IsCancellationRequested)
+            if (!tryPowerOnCancellationTokenSource.IsCancellationRequested)
             {
-                outputPin.Write(PinValue.Low);
-                await Task.Delay(TimeSpan.FromSeconds(5), powerOnCancellationTokenSource.Token);
+                SetTryPowerOnOutputPinValue(PinValue.Low);
+                await Task.Delay(TimeSpan.FromSeconds(5), tryPowerOnCancellationTokenSource.Token);
             }
-            if (!powerOnCancellationTokenSource.IsCancellationRequested)
+            if (!tryPowerOnCancellationTokenSource.IsCancellationRequested)
             {
-                outputPin.Write(PinValue.High);
-                await Task.Delay(TimeSpan.FromSeconds(1), powerOnCancellationTokenSource.Token);
+                if (IsPlayStationPowerOff())
+                {
+                    SetTryPowerOnOutputPinValue(PinValue.High);
+                    await Task.Delay(TimeSpan.FromSeconds(1), tryPowerOnCancellationTokenSource.Token);
+                }
             }
         }
-        outputPin.Write(PinValue.Low);
+        // The thread was cancelled - turn off the power on output pin
+        SetTryPowerOnOutputPinValue(PinValue.Low);
+    }
+
+    private bool IsPlayStationPowerOff()
+    {
+        var value = inputPin.Read();
+        return value == PinValue.Low;
     }
 
     private void OnPlayStationPowerInputPinValueChanged(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
     {
-        Console.WriteLine("{0} : PlayStationPowerInputPin value {1}", DateTime.Now.ToString("s"), pinValueChangedEventArgs.ChangeType);
+        inputPinValueChangesSubject.OnNext(pinValueChangedEventArgs);
     }
 }
